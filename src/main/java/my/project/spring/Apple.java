858eb7559a4d0f67f44b241795e4357fa7877408
Diana <kpds7175@gmail.com>
2019-06-25 16:56:30 +0300

    init

---
+++ src/main/java/my/project/spring/Apple.java
@@ -0,0 +1,13 @@
+package my.project.spring;
+
+public class Apple {
+
+        private final Integer weight;
+        private final String color;
+
+
+    public Apple(Integer weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+}
